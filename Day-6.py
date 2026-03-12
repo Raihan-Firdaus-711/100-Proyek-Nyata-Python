9162e47ee43a51ed2ce39a8e81f9fa4cbf758681
Raihan-Firdaus-711 <firdausrehan0@gmail.com>
2026-03-12 14:26:50 +0700

    Add functions to greet and get user name

---
+++ Day-6.py
@@ -0,0 +1,10 @@
+def helloword():
+    print("Hello")
+    print("Word!")
+helloword()
+
+def get_user_name():
+    name = input("What is your name?")
+    print(f"Hello, {name}")
+print("Hello")
+get_user_name()
